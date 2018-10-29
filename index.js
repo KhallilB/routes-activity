cdd7608a0c7395906322e3d087c5b53c0b6e1da1
KhallilB <khallil.bailey@gmail.com>
2018-10-29 15:11:30 -0700

    added a post route

--- index.js
+++ index.js
@@ -19,23 +19,36 @@ const Event = [
 	{
 		id: "0",
 		name: "Pumpkin Party",
-		day: "10/31/18",
+		date: "10/31/18",
 		type: "Music Festival"
 	},
 	{
 		id: "1",
 		name: "New Year's Code",
-		day: "01/01/19",
+		date: "01/01/19",
 		type: "Hackathon"
 	}
 ];
 
-app.get("/", (request, response) => { // Send all venues.
-	response.send(Venue)
+app.get("/venues", (request, response) => { // Send all venues.
+	response.json(Venue)
 });
 
-app.get("/:id", (req, res) => { // Send a venue that matches a specific id.
-	res.send(Venue[req.params.id])
+app.get("/venues/:id", (req, res) => { // Send a venue that matches a specific id.
+	res.json(Venue[req.params.id])
+})
+
+app.get("/venues/:id/events", (req, res) => {
+	res.json(Event);
+})
+
+app.post("/venues/:id/events/new", (req, res) => {
+	Event.push({
+	id: "3",
+	name: "Birthday Event",
+	date: "3/23/19",
+	type: "Birthday"
+});
 })
 
 app.listen(3000, () => {
