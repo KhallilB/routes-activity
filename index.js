8c1edb3dcaacfdb031fa7baf745d9b9d61968503
Nolan Kovacik <noltron000@gmail.com>
2018-10-29 14:28:06 -0700

    added data

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
-const express require('express');
-const EventRoute = express.Router();
+const express require('express'); // Import express
+const EventRoute = express.Router(); // Set up Router
 
-let Venue = {}
 
-module.exports = EventRoute
+const Venue = [
+	{
+		id: "0",
+		name: "Super Test Venue",
+		location: "San Francisco"
+	},
+	{
+		id: "1",
+		name: "Amazing Venue",
+		location: "San Jose"
+	}
+];
+
+const Event = [
+	{
+		id: "0",
+		name: "Pumpkin Party",
+		day: "10/31/18",
+		type: "Music Festival"
+	},
+	{
+		id: "1",
+		name: "New Year's Code",
+		day: "01/01/19",
+		type: "Hackathon"
+	}
+];
+
+express.get("./venues", (request, response) => {
+
+})
+
+
+
+module.exports = EventRoute // Export Router
